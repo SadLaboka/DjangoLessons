9d43d4b74410f4f20774b38c666c625d48fb0583
SadLaboka <toteshephysic@yandex.ru>
2021-01-15 14:14:55 +0300

    added Category model, worked with db and admin panel

--- testproject/testsite/news/models.py
+++ testproject/testsite/news/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -16,3 +17,16 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование категории', db_index=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
